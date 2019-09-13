2a7af4644bb718ad1ec6acc337666b10cf5c8a92
ajithcravi <craviajith@gmail.com>
2019-09-13 11:36:54 +0530

    Basic 3 pages made

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('body').fadeIn(3000);
+    $('body').fadeOut(2500);
+  });
+
+  $(document).ready(function () {
+    window.setTimeout(function () {
+        location.href = "https://www.google.co.in";
+    }, 6000);
+    });
