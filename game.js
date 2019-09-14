c2f2b4ac0e68bebccba501e158e42a357d206ef1
parvez <sk.parvezshah24@gmail.com>
2019-09-14 14:40:24 +0530

    updated java script and json

--- game.js
+++ game.js
@@ -1,104 +1,3 @@
-
-// Switching
-let displayOne = document.getElementsByClassName("displayOne");
-let displayTwo = document.getElementsByClassName("displayTwo");
-let displayThree = document.getElementsByClassName("displayThree");
-let displayQuestion = document.getElementsByClassName("displayQuestion");
-
-function switchDiv(flag){
-  switch (flag) {
-    case 2:
-      displayOne[0].style.display = "none";
-      displayTwo[0].style.display = "block";
-      break;
-
-    case 3:
-      displayTwo[0].style.display = "none";
-      displayThree[0].style.display = "block";
-      break;
-
-    case 4:
-      displayThree[0].style.display = "none";
-      displayQuestion[0].style.display = "block";
-      break;
-  }
-}
-
-
-
-
-// Navigation Bar
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
-
-// Timer
-//set minutes
-var mins = 10;
-//calculate the seconds
-var secs = mins * 60;
-//countdown function is evoked when page is loaded
-function countdown() {
-    setTimeout('Decrement()', 60);
-}
-//Decrement function decrement the value.
-function Decrement() {
-    if (document.getElementById) {
-        minutes = document.getElementById("minutes");
-        seconds = document.getElementById("seconds");
-        //if less than a minute remaining
-        //Display only seconds value.
-        if (seconds < 59) {
-            seconds.value = secs;
-        }
-        //Display both minutes and seconds
-        //getminutes and getseconds is used to
-        //get minutes and seconds
-        else {
-            minutes.value = getminutes();
-            seconds.value = getseconds();
-        }
-        //when less than a minute remaining
-        //colour of the minutes and seconds
-        //changes to red
-        // if (mins < 1) {
-        //     minutes.style.color = "red";
-        //     seconds.style.color = "red";
-        // }
-        //if seconds becomes zero,
-        //then page alert time up
-        // if (mins < 0) {
-        //     alert('time up');
-        //     minutes.value = 0;
-        //     seconds.value = 0;
-        // }
-        //if seconds > 0 then seconds is decremented
-        if(secs>0) {
-            secs--;
-            setTimeout('Decrement()', 1000);
-        }
-    }
-}
-function getminutes() {
-    //minutes is seconds divided by 60, rounded down
-    mins = Math.floor(secs / 60);
-    return mins;
-}
-function getseconds() {
-    //take minutes remaining (as seconds) away
-    //from total seconds remaining
-    return secs - Math.round(mins * 60);
-}
-
-
-
-// the game test starts here
-
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
@@ -149,8 +48,8 @@ fetch(
   });
 
 //CONSTANTS
-const CORRECT_BONUS = 2;
-const MAX_QUESTIONS = 20;
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
   questionCounter = 0;
@@ -213,16 +112,3 @@ incrementScore = num => {
   score += num;
   scoreText.innerText = score;
 };
-
-
-
-// the highscore container
-
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highScoresList.innerHTML = highScores
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
